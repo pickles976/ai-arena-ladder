45d5e4496be61e4de61b7994634d047c9926a87c
unknown <sebaslogo@gmail.com>
2023-07-30 13:39:22 -0500

    Added env vars

--- multiplayer/start_game.sh
+++ multiplayer/start_game.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+DROPLET_IP=$DROPLET_IP
+SUPABASE_SECRET_KEY=$SUPABASE_SECRET_KEY
+
 killall beanstalkd
 killall node
 
 echo "Starting Beanstalkd server..."
-beanstalkd -l localhost -p 11300
+beanstalkd -l localhost -p 11300 &
 
 echo "Starting campaign server..."
 cd campaign_server && node --es-module-specifier-resolution=node ./campaign.js &
-cd ..
 
 echo "Waiting for campaign server initialization..."
 sleep 5
 
 echo "Starting battle server..."
-cd battle_server && node --es-module-specifier-resolution=node ./battle.js &
-cd ..
+cd ../battle_server && node --es-module-specifier-resolution=node ./battle.js &
 
 echo "Done!"
