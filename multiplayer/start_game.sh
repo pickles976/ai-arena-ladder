618ab9b6fde6072fd1eb0fc0337f8a00c4c0800d
unknown <sebaslogo@gmail.com>
2023-07-30 13:27:26 -0500

    updated bash script

--- multiplayer/start_game.sh
+++ multiplayer/start_game.sh
@@ -7,11 +7,14 @@ echo "Starting Beanstalkd server..."
 beanstalkd -l localhost -p 11300
 
 echo "Starting campaign server..."
-cd campaign_server && node --es-module-specifier-resolution=node ./campaign.js
+cd campaign_server && node --es-module-specifier-resolution=node ./campaign.js &
 cd ..
 
+echo "Waiting for campaign server initialization..."
+sleep 5
+
 echo "Starting battle server..."
-cd battle_server && node --es-module-specifier-resolution=node ./battle.js
+cd battle_server && node --es-module-specifier-resolution=node ./battle.js &
 cd ..
 
 echo "Done!"
