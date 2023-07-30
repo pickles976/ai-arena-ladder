92298086b23fa63135199a97dda903fe85c2f1c7
unknown <sebaslogo@gmail.com>
2023-07-30 13:50:46 -0500

    Added stuff to bash scirpt again

--- multiplayer/start_game.sh
+++ multiplayer/start_game.sh
@@ -10,15 +10,15 @@ killall beanstalkd
 killall node
 
 echo "Starting Beanstalkd server..."
-beanstalkd -l localhost -p 11300 &
+beanstalkd -l 127.0.0.1 -p 11300 &
 
 echo "Starting campaign server..."
-cd campaign_server && node --es-module-specifier-resolution=node ./campaign.js &
+node --es-module-specifier-resolution=node ./campaign_server/campaign.js &
 
 echo "Waiting for campaign server initialization..."
 sleep 5
 
 echo "Starting battle server..."
-cd ../battle_server && node --es-module-specifier-resolution=node ./battle.js &
+node --es-module-specifier-resolution=node ./battle_server/battle.js &
 
 echo "Done!"
